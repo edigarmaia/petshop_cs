8654b8609e88b593b8b4a14bfe4900fb732b2925
Edigar Maia <edigarmaia@gmail.com>
2023-09-23 20:08:51 -0300

    Criado as configuracoes de rotas

--- Petshop/petshop/src/main/java/br/com/unicsul/petshop/PetshopController.java
+++ Petshop/petshop/src/main/java/br/com/unicsul/petshop/PetshopController.java
@@ -1,4 +1,24 @@
 package br.com.unicsul.petshop;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class PetshopController {
+
+    @GetMapping("/servicos")
+    public String rotaServicos() {
+        return "servicos";
+    }
+
+    @GetMapping("/quemsomos")
+    public String rotaQuemSomos(){
+        return "quemsomos";
+    }
+
+    @GetMapping("/saudacao/{animal}")
+    public String rotaSaudacao(@PathVariable String animal, ModelMap model) {
+        model.addAttribute("animal", animal);
+        return "saudacao";
+    }
 }
