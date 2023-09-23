3607d9cfcde12de4e45b7a955c462d0b0a45e553
Edigar Maia <edigarmaia@gmail.com>
2023-09-23 20:13:30 -0300

    Criacao das paginas index, servicos, quem somos e saudacao

--- Petshop/petshop/src/main/java/br/com/unicsul/petshop/PetshopController.java
+++ Petshop/petshop/src/main/java/br/com/unicsul/petshop/PetshopController.java
@@ -1,9 +1,13 @@
 package br.com.unicsul.petshop;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
+@SpringBootApplication
 public class PetshopController {
 
     @GetMapping("/servicos")
